6b9ad41d445490d73e3793654b8ad6cd879836cc
root <root@localhost.localdomain>
2018-08-01 14:51:03 -0400

    update chmod and add services.sh

---
+++ install/services.sh
@@ -0,0 +1,13 @@
+#cp tomcat /etc/init.d/
+#ln -s /etc/init.d/tomcat /etc/rc2.d/S90tomcat
+#/etc/init.d/tomcat start
+
+cp redis /etc/init.d/
+ln -s /etc/init.d/redis /etc/rc2.d/S90redis
+
+cp activemq /etc/init.d/
+ln -s /etc/init.d/activemq /etc/rc2.d/S90activemq
+
+cp elasticsearch /etc/init.d/
+ln -s /etc/init.d/elasticsearch /etc/rc2.d/S90elasticsearch
+
