d914f93476e51cbdcff13c9c952db21c890d846d
tsegay <tsegay@users.noreply.github.com>
2018-08-02 09:46:09 -0400

    Update install_elasticsearch.sh

--- install/install_elasticsearch.sh
+++ install/install_elasticsearch.sh
@@ -8,23 +8,22 @@
 #
 
 # Set environment variable to elastic search 
-export ES_HOME="/usr/local/elasticsearch"
-
-# Download Elasticsearch v2.3.2
-wget https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/2.3.2/elasticsearch-2.3.2.tar.gz
-
-tar xvf elasticsearch-2.3.2.tar.gz
-
-cp -r elasticsearch-2.3.2 /usr/local
-
-cd /usr/local
-
-# Soft link elasticsearch -> elasticsearch-2.3.2, this way when the elasticsearch version is updated, we would only need to update the tomcat link
-ln -s elasticsearch-2.3.2 elasticsearch
+export ES_HOME="/home/gtas/elasticsearch"
 
 useradd gtas
 passwd gtas
 
 usermod -aG wheel gtas
 
-chown -R gtas:gtas /usr/local/elasticsearch/*
+# Download Elasticsearch v2.3.2
+wget https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/2.3.2/elasticsearch-2.3.2.tar.gz
+
+tar xvf elasticsearch-2.3.2.tar.gz
+
+cp -r elasticsearch-2.3.2 /home/gtas/
+
+cd /home/gtas/
+
+# Soft link elasticsearch -> elasticsearch-2.3.2, this way when the elasticsearch version is updated, we would only need to update the tomcat link
+ln -s elasticsearch-2.3.2 elasticsearch
+
