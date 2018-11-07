6a8e2a857b20d733ec5c60496d652d28725d1d7b
Tsegay <tsegay.tekle@gmail.com>
2018-11-07 16:46:08 -0500

    update tomcat version to apache-tomcat-8.5.34

--- install/install_tomcat.sh
+++ install/install_tomcat.sh
@@ -13,17 +13,17 @@
     wget http://us.mirrors.quenda.co/apache/tomcat/tomcat-8/v8.5.34/bin/apache-tomcat-8.5.34.tar.gz
 
 # unzip the tar
-    tar xvf apache-tomcat-8.5.32.tar.gz
+    tar xvf apache-tomcat-8.5.34.tar.gz
 
 # copy/move the extracted folder to /usr/local
-    cp -r apache-tomcat-8.5.32 /usr/local
+    cp -r apache-tomcat-8.5.34 /usr/local
 
 cd /usr/local
 
 
 # create soft link tomcat -> apache-tomcat-8.5.32, this way when the tomcat version is updated we would only need to update the tomcat link
 
-ln -s /usr/local/apache-tomcat-8.5.32 /usr/local/tomcat
+ln -s /usr/local/apache-tomcat-8.5.34 /usr/local/tomcat
 
 firewall-cmd --permanent --zone=public --add-port=8080/tcp
 
