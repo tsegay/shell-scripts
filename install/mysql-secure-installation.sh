d0d752e66a94595d101d34194f3711c4ac37f6e1
Tsegay <tsegay.tekle@gmail.com>
2019-04-03 10:50:09 -0400

    manually run mysql secure installation, change password to admin

--- install/mysql-secure-installation.sh
+++ install/mysql-secure-installation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Make sure that NOBODY can access the server without a password
-mysql -e "UPDATE mysql.user SET Password = PASSWORD('CHANGEME') WHERE User = 'root'"
+mysql -e "UPDATE mysql.user SET Password = PASSWORD('admin') WHERE User = 'root'"
 # Kill the anonymous users
 mysql -e "DROP USER ''@'localhost'"
 # Because our hostname varies we'll use some Bash magic here.
