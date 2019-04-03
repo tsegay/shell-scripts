ea1be566b4d297d5e69e65cc7ec69ac922c69bf0
Tsegay <tsegay.tekle@gmail.com>
2019-04-03 10:37:04 -0400

    manually run mysql secure installation

---
+++ install/mysql-secure-installation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Make sure that NOBODY can access the server without a password
+mysql -e "UPDATE mysql.user SET Password = PASSWORD('CHANGEME') WHERE User = 'root'"
+# Kill the anonymous users
+mysql -e "DROP USER ''@'localhost'"
+# Because our hostname varies we'll use some Bash magic here.
+mysql -e "DROP USER ''@'$(hostname)'"
+# Kill off the demo database
+mysql -e "DROP DATABASE test"
+# Make our changes take effect
+mysql -e "FLUSH PRIVILEGES"
+# Any subsequent tries to run queries this way will get access denied because lack of usr/pwd param
