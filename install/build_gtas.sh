944546a548601e7702ff010000cf2bcd685a0d7d
tsegay <tsegay@users.noreply.github.com>
2019-07-09 12:52:16 -0400

    Update build_gtas.sh
    run scripts as the current user.

--- install/build_gtas.sh
+++ install/build_gtas.sh
@@ -1,15 +1,15 @@
 
-GTAS_DEV=/home/gtas/repository
+GTAS_DEV=/home/$(whoami)/repository
 
 # Checkout GTAS dev branch from github repository
-sudo -u gtas git clone --single-branch --branch dev https://github.com/US-CBP/GTAS.git $GTAS_DEV/
+sudo -u $(whoami) git clone --single-branch --branch dev https://github.com/US-CBP/GTAS.git $GTAS_DEV/
 
 # add mvn install
 cd $GTAS_DEV/gtas-parent
-sudo -u gtas mvn clean install -Dskip.unit.tests=true
+sudo -u $(whoami) mvn clean install -Dskip.unit.tests=true
 
 # run GTAS database script
 ## add createDatabaseIfNotExist=true to the mysql connection string
 cd $GTAS_DEV/gtas-parent/gtas-commons/
-sudo -u gtas mvn hibernate:drop
-sudo -u gtas mvn hibernate:create
+sudo -u $(whoami) mvn hibernate:drop
+sudo -u $(whoami) mvn hibernate:create
