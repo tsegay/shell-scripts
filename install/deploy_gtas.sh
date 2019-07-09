ee821270caf861bb354cd484b3a45d76786d00b4
tsegay <tsegay@users.noreply.github.com>
2019-07-09 13:05:55 -0400

    Update deploy_gtas.sh
    run the scripts as the current user.

--- install/deploy_gtas.sh
+++ install/deploy_gtas.sh
@@ -1,10 +1,10 @@
 
-GTAS_DEV=/home/gtas/repository
+GTAS_DEV=/home/$(whoami)/repository
 
 cd $GTAS_DEV/gtas-parent/gtas-webapp
 
-sudo -u gtas /usr/local/maven/bin/mvn initialize tomcat7:deploy
+sudo -u $(whoami) /usr/local/maven/bin/mvn initialize tomcat7:deploy
 
 cd $GTAS_DEV/gtas-parent/gtas-job-scheduler-war
 
-sudo -u gtas /usr/local/maven/bin/mvn initialize tomcat7:deploy
+sudo -u $(whoami) /usr/local/maven/bin/mvn initialize tomcat7:deploy
